7e5a7ab3dce4881c860606ddbe1abbe51b8941d2
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-16 19:04:37 +0100

    set the stars value to an empty array

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
+  console.log(props)
 
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -30,7 +31,8 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  const { title, director, metascore, stars = [] } = movie;
+
   return (
     <div className="save-wrapper">
       <div className="movie-card">
